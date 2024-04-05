017d7c3ff997abba70dd984a9ab244e4ff6b17f2
Yosua Nicolaus <yoni@odoo.com>
2024-04-05 18:48:23 +0200

    add debug template

--- template/min.cpp
+++ template/min.cpp
@@ -1,19 +1,22 @@
+#if LOCAL
+#include "template/debug.h"
+#else
+#define debug(...)
+#endif
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
 void solve() {
-    /* solve the problem */
+    /* ~ good luck! ~ */
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int t;
     cin >> t;
     while (t--)
         solve();
-
     return 0;
 }
