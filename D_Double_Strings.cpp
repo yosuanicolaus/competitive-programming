9176ce726a79748adcf3437cf41cee4b960131a1
Yosua Nicolaus <yoni@odoo.com>
2024-04-10 23:50:03 +0200

    practice 240410

---
+++ D_Double_Strings.cpp
@@ -0,0 +1,52 @@
+#if LOCAL
+#include "template/debug.h"
+#else /*©[yosuanicolaus]©*/
+#define d(...)
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+auto solve() {
+    int n;
+    cin >> n;
+    vector<string> vecstr(n);
+    unordered_set<string> setstr;
+    string ans;
+
+    for (int i = 0; i < n; i++) {
+        cin >> vecstr[i];
+        setstr.insert(vecstr[i]);
+    }
+
+    for (const auto &str : vecstr) {
+        int len = str.size();
+        if (len == 1) {
+            ans += '0';
+            continue;
+        }
+        bool found = false;
+        for (int i = 1; i < len; i++) {
+            string first_word = str.substr(0, i);
+            string second_word = str.substr(i, 8);
+            if (setstr.count(first_word) && setstr.count(second_word)) {
+                ans += '1';
+                found = true;
+                break;
+            }
+        }
+        if (!found) ans += '0';
+    }
+
+    return ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+        cout << solve() << '\n';
+    return 0;
+}
